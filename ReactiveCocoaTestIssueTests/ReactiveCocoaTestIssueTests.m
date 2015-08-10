047984a1c64487be08911b9d70d9df8c20e7d771
Liquidsoul <liquidsoul@liquidsoul.fr>
2015-08-10 13:23:38 +0200

    Failing test because of unintended RAC assert
    Test target is compiled using the main target pod configuration. Maybe
    this is what is causing the issue.

--- ReactiveCocoaTestIssueTests/ReactiveCocoaTestIssueTests.m
+++ ReactiveCocoaTestIssueTests/ReactiveCocoaTestIssueTests.m
@@ -7,6 +7,9 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+#import "ViewModel.h"
 
 @interface ReactiveCocoaTestIssueTests : XCTestCase
 
@@ -16,24 +19,15 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testExecute {
+	ViewModel *viewModel = [ViewModel new];
+	[viewModel.executeSearch execute:nil];
 }
 
 @end
